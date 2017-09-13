ee5c7fec50c10b74297a7ebf9b8d4e402ad25ffc
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-09-13 17:27:40 +0200

    Add TestClass for MaterialityGrid

--- Materiality/Materiality.Tests/MaterialityGridTests.cs
+++ Materiality/Materiality.Tests/MaterialityGridTests.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Materiality.WPF.Layouts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Materiality.Tests
 {
     [TestClass]
     public class MaterialityGridTests
     {
-        
+        [TestMethod]
+        [Description("No matter what kind of layout is being arranged, the result is True")]
+        public void FitsControlsProperly()
+        {
+            //arrange
+            MaterialityGrid grid = new MaterialityGrid();
+            //act
+
+            //assert
+        }
     }
 }
