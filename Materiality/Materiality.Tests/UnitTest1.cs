bda9e6b910315940d5cf69e27e39cc2e853222cf
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-09-13 12:45:13 +0200

    Add Test Project

---
+++ Materiality/Materiality.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Materiality.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
