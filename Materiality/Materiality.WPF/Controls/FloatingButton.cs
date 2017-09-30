a0072d2e9de7386e6646c04602c5b01d6d23b6c0
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-09-30 15:44:24 +0200

    Add FloatingButton

---
+++ Materiality/Materiality.WPF/Controls/FloatingButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Materiality.WPF.Controls
+{
+    public class FloatingButton : Button
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(FloatingButton), new PropertyMetadata("NEW BUTTON"));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty HoverBrushProperty = DependencyProperty.Register("HoverBrush", typeof(Brush), typeof(FloatingButton), new UIPropertyMetadata(Brushes.LightGray));
+        public Brush HoverBrush
+        {
+            get { return (Brush)GetValue(HoverBrushProperty); }
+            set { SetValue(HoverBrushProperty, value); }
+        }
+
+        static FloatingButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FloatingButton), new FrameworkPropertyMetadata(typeof(FloatingButton)));
+        }
+    }
+}
