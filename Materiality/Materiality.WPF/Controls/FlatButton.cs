7b7d6cecf34dc6599dc7255474d1bbb7aa501cef
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-09-30 14:53:16 +0200

    Add FlatButton

---
+++ Materiality/Materiality.WPF/Controls/FlatButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Materiality.WPF.Controls
+{
+    public class FlatButton : Button
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(FlatButton), new PropertyMetadata("NEW BUTTON"));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty HoverBrushProperty = DependencyProperty.Register("HoverBrush", typeof(Brush), typeof(FlatButton), new UIPropertyMetadata(Brushes.LightGray));
+        public Brush HoverBrush
+        {
+            get { return (Brush)GetValue(HoverBrushProperty); }
+            set { SetValue(HoverBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty HoverTextBrushProperty = DependencyProperty.Register("HoverTextBrush", typeof(Brush), typeof(FlatButton), new UIPropertyMetadata(Brushes.Black));
+        public Brush HoverTextBrush
+        {
+            get { return (Brush)GetValue(HoverTextBrushProperty); }
+            set { SetValue(HoverTextBrushProperty, value); }
+        }
+
+        static FlatButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatButton), new FrameworkPropertyMetadata(typeof(FlatButton)));
+        }
+
+    }
+}
