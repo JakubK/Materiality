1ec39d52fd079b3fd3de8a3f981bc4cacb3372de
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-09-30 14:52:59 +0200

    Rename MaterialButton to RaisedButton

---
+++ Materiality/Materiality.WPF/Controls/RaisedButton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Materiality.WPF.Controls
+{
+    public class RaisedButton : Button
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(RaisedButton), new PropertyMetadata("NEW BUTTON"));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty HoverBrushProperty = DependencyProperty.Register("HoverBrush",typeof(Brush),typeof(RaisedButton), new UIPropertyMetadata(Brushes.White));
+        public Brush HoverBrush
+        {
+            get { return (Brush)GetValue(HoverBrushProperty); }
+            set { SetValue(HoverBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty RippleBrushProperty = DependencyProperty.Register("RippleBrush", typeof(Brush), typeof(RaisedButton), new UIPropertyMetadata(Brushes.White));
+        public Brush RippleBrush
+        {
+            get { return (Brush)GetValue(RippleBrushProperty); }
+            set { SetValue(RippleBrushProperty, value); }
+        }
+
+        static RaisedButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RaisedButton), new FrameworkPropertyMetadata(typeof(RaisedButton)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.Click += MaterialButton_Click;
+        }
+
+        private void MaterialButton_Click(object sender, RoutedEventArgs e)
+        {
+            Ripple ripple = GetTemplateChild("Ripple") as Ripple;
+            ripple.ScaleUpRipple();
+        }
+    }
+}
