d9dfac7bc58d8bb6eed4ba661e61e8a752e96aba
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-10-03 21:17:01 +0200

    Add Toolbar

---
+++ Materiality/Materiality.WPF/Controls/Toolbar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Materiality.WPF.Controls
+{
+    public class Toolbar : ItemsControl
+    {
+        static Toolbar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Toolbar), new FrameworkPropertyMetadata(typeof(Toolbar)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+    }
+}
