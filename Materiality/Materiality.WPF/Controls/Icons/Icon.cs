0fb27401ac113d8de6733886d753b8529ea5cb44
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-10-08 13:42:18 +0200

    Create Icon Control for using Glyphs in Materiality
    It also includes NamesToPathConverter which returns PathData by Icon Name

---
+++ Materiality/Materiality.WPF/Controls/Icons/Icon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.ComponentModel;
+using Materiality.WPF.Classes.Icons;
+
+namespace Materiality.WPF.Controls
+{
+    public class Icon : ContentControl
+    {
+        public Path IconPath; 
+
+        static Icon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Icon), new FrameworkPropertyMetadata(typeof(Icon)));
+        }
+
+        public static readonly DependencyProperty IconTypeProperty = DependencyProperty.Register("Type", typeof(IconType), typeof(Icon), new FrameworkPropertyMetadata(IconType.AccessPoint));
+        public IconType Type
+        {
+            get { return (IconType)GetValue(IconTypeProperty); }
+            set { SetValue(IconTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty FillProperty = DependencyProperty.Register("Fill", typeof(Brush), typeof(Icon), new FrameworkPropertyMetadata(Brushes.Black));
+        public Brush Fill
+        {
+            get { return (Brush)GetValue(FillProperty); }
+            set { SetValue(FillProperty, value); }
+        }
+
+        public Icon()
+        {
+            IconPath = new Path();
+            //Path path = new Path();
+            //string sData = "M 250,40 L200,20 L200,60 Z";
+            //var converter = TypeDescriptor.GetConverter(typeof(Geometry));
+            //path.Data = (Geometry)converter.ConvertFrom(sData);
+        }
+    }
+}
---
+++ Materiality/Materiality.WPF/Converters/NameToPathConverter.cs
@@ -0,0 +1,26 @@
+﻿using Materiality.WPF.Classes.Icons;
+using Materiality.WPF.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Materiality.WPF.Converters
+{
+    public class NameToPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            IconType type = (IconType)value;
+            return IconHelper.IconDictionary[type];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
