acb37340ff30579013ea989fe7a007a3bc4550c6
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-10-19 13:56:09 +0200

    Add DurationSumConverter and Duration2TimeSpanConverter

---
+++ Materiality/Materiality.WPF/Converters/DurationSumConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Materiality.WPF.Converters
+{
+    public class DurationSumConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new TimeSpan(0, 0, 0, ((Duration)values[0]).TimeSpan.Seconds + ((Duration)values[1]).TimeSpan.Seconds, ((Duration)values[0]).TimeSpan.Milliseconds + ((Duration)values[1]).TimeSpan.Milliseconds); 
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
