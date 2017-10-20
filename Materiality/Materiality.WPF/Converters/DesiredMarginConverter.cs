c4cb33f03aa0f387ac164f1cc1abb83eecc105a5
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-10-20 15:05:06 +0200

    Add DesiredMarginConverter for calculating DesiredMargin for Switch

---
+++ Materiality/Materiality.WPF/Converters/DesiredMarginConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Materiality.WPF.Converters
+{
+    public class DesiredMarginConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double mainActualWidth = (double)values[0];
+            double toggleActualWidth = (double)values[1];
+            return new Thickness(mainActualWidth - toggleActualWidth, 0, 0, 0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
