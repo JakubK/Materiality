13c600c21e2dcc1092925129b53ad3ab62eccffc
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2017-11-11 17:46:08 +0100

    Add FlowTextBlock to the Materiality.WPF

---
+++ Materiality/Materiality.WPF/Controls/Typography/FlowTextBlock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Materiality.WPF.Controls
+{
+    public class FlowTextBlock : ContentControl
+    {
+        static FlowTextBlock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FlowTextBlock), new FrameworkPropertyMetadata(typeof(FlowTextBlock)));
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(FlowTextBlock), new PropertyMetadata());
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
